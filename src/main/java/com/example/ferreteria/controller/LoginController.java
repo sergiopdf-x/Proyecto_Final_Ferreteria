95fcd220e9ebace3fcd0c48fcc38ba9a02dc13c0
sergiopdf-x <stoapanta887@gmail.com>
2026-06-25 17:04:46 +0200

    Implementacion de navegacion ente formularios en LoginController y aplicacion de estilos

--- src/main/java/com/example/ferreteria/controller/LoginController.java
+++ src/main/java/com/example/ferreteria/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.example.ferreteria.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -14,7 +16,7 @@ public class LoginController {
     @FXML
     private Button btnIngresar;
 
-    private void alerta(String titulo, String mensaje, Alert.AlertType tipo){
+    private void alerta(String titulo, String mensaje, Alert.AlertType tipo) {
         Alert alerta = new Alert(tipo);
         alerta.setTitle(titulo);
         alerta.setHeaderText(null);
@@ -22,20 +24,46 @@ public class LoginController {
         alerta.showAndWait();
     }
 
+    private void navegarEntreFormularios(String rutaFxml, String titulo) {
+        try {
+            javafx.fxml.FXMLLoader loader = new javafx.fxml.FXMLLoader(getClass().getResource(rutaFxml));
+            javafx.scene.Parent root = loader.load();
+
+            //Obtener la pagina actual utilizando el boton ingresar y cambio a la nueva pagina
+            javafx.stage.Stage stage = (javafx.stage.Stage) txtUsuario.getScene().getWindow();
+            stage.setScene(new javafx.scene.Scene(root));
+            stage.setTitle("Ferreteria - " + titulo);
+            stage.show();
+
+        } catch (Exception e) {
+            alerta("Error", "No fue posible cambiar la pantalla: " + e.getMessage(), Alert.AlertType.ERROR);
+            e.printStackTrace();
+        }
+    }
+
     @FXML
-    private void manejarLogin(){
+    private void manejarLogin() {
         String usuario = txtUsuario.getText().trim();
         String contra = txtContra.getText().trim();
 
-        if(usuario.isEmpty() || contra.isEmpty()){
-            alerta("Campos vacios","Usuario o contraseña no pueden estar vacios",Alert.AlertType.WARNING);
+        if (usuario.isEmpty() || contra.isEmpty()) {
+            alerta("Campos vacios", "Usuario o contraseña no pueden estar vacios", Alert.AlertType.WARNING);
             return;
         }
 
-        if(contra.length() < 6){
-            alerta("Contraseña invalida","La contraseña debe tener mínimo 6 caracteres",Alert.AlertType.WARNING);
+        if (contra.length() < 6) {
+            alerta("Contraseña invalida", "La contraseña debe tener mínimo 6 caracteres", Alert.AlertType.WARNING);
             return;
         }
-        alerta("Exito","Validaciones aprobadas.",Alert.AlertType.INFORMATION);
+
+        if (usuario.equals("admin@poli.com") && contra.equals("admin123")) {
+            alerta("Exito", "Validaciones aprobadas. Bienvenido Administrador", Alert.AlertType.INFORMATION);
+            navegarEntreFormularios("/com/example/ferreteria/dashboard.fxml", "Dashboard Principal");
+        } else if (usuario.equals("bodega@poli.com") && contra.equals("bodega123")) {
+            alerta("Exito", "Bienvenido bodeguero", Alert.AlertType.INFORMATION);
+            navegarEntreFormularios("/com/example/ferreteria/productos.fxml", "Gestion de Inventario");
+        } else {
+            alerta("Error", "Credenciales Incorrectas", Alert.AlertType.ERROR);
+        }
     }
 }
